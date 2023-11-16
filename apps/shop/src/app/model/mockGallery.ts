831f187cc7d43bac19fc2f3d2c5cb358a28473ac
Carlos Silva <55637586+carlossilva22@users.noreply.github.com>
2023-11-16 20:06:37 +0000

    alterar o imagem

--- apps/shop/src/app/model/mockGallery.ts
+++ apps/shop/src/app/model/mockGallery.ts
@@ -3,201 +3,201 @@ import { Gallery } from './gallery';
 export const Gallarias: Gallery[] = [
   {
     id: 1,
-    image: './assets/galaria/_DSC0020.png',
+    image: './assets/galaria/DSC0020.png',
   },
 
   {
     id: 2,
-    image: './assets/galaria/_DSC0027.png',
+    image: './assets/galaria/DSC0027.png',
   },
   {
     id: 3,
-    image: './assets/galaria/_DSC0031.png',
+    image: './assets/galaria/DSC0031.png',
   },
   {
     id: 4,
-    image: './assets/galaria/_DSC0035.png',
+    image: './assets/galaria/DSC0035.png',
   },
 
   {
     id: 5,
-    image: './assets/galaria/_DSC0041.png',
+    image: './assets/galaria/DSC0041.png',
   },
 
   {
     id: 6,
-    image: './assets/galaria/_DSC0044.png',
+    image: './assets/galaria/DSC0044.png',
   },
   {
     id: 7,
-    image: './assets/galaria/_DSC0047.png',
+    image: './assets/galaria/DSC0047.png',
   },
 
   {
     id: 8,
-    image: './assets/galaria/_DSC0051.png',
+    image: './assets/galaria/DSC0051.png',
   },
 
   {
     id: 9,
-    image: './assets/galaria/_DSC0054.png',
+    image: './assets/galaria/DSC0054.png',
   },
 
   {
     id: 10,
-    image: './assets/galaria/_DSC0066.png',
+    image: './assets/galaria/DSC0066.png',
   },
 
   {
     id: 11,
-    image: './assets/galaria/_DSC0074.png',
+    image: './assets/galaria/DSC0074.png',
   },
 
   {
     id: 12,
-    image: './assets/galaria/_DSC0082.png',
+    image: './assets/galaria/DSC0082.png',
   },
   {
     id: 13,
-    image: './assets/galaria/_DSC0087.png',
+    image: './assets/galaria/DSC0087.png',
   },
   {
     id: 14,
-    image: './assets/galaria/_DSC0090.png',
+    image: './assets/galaria/DSC0090.png',
   },
 
   {
     id: 15,
-    image: './assets/galaria/_DSC0092.png',
+    image: './assets/galaria/DSC0092.png',
   },
   {
     id: 16,
-    image: './assets/galaria/_DSC0093.png',
+    image: './assets/galaria/DSC0093.png',
   },
 
   {
     id: 17,
-    image: './assets/galaria/_DSC0096.png',
+    image: './assets/galaria/DSC0096.png',
   },
 
   {
     id: 18,
-    image: './assets/galaria/_DSC0102.png',
+    image: './assets/galaria/DSC0102.png',
   },
   {
     id: 19,
-    image: './assets/galaria/_DSC0103.png',
+    image: './assets/galaria/DSC0103.png',
   },
 
   {
     id: 20,
-    image: './assets/galaria/_DSC0111.png',
+    image: './assets/galaria/DSC0111.png',
   },
 
   {
     id: 21,
-    image: './assets/galaria/_DSC0125.png',
+    image: './assets/galaria/DSC0125.png',
   },
 
   {
     id: 22,
-    image: './assets/galaria/_DSC0131.png',
+    image: './assets/galaria/DSC0131.png',
   },
 
   {
     id: 23,
-    image: './assets/galaria/_DSC0135.png',
+    image: './assets/galaria/DSC0135.png',
   },
   {
     id: 24,
-    image: './assets/galaria/_DSC0142.png',
+    image: './assets/galaria/DSC0142.png',
   },
 
   {
     id: 25,
-    image: './assets/galaria/_DSC0148.png',
+    image: './assets/galaria/DSC0148.png',
   },
   {
     id: 26,
-    image: './assets/galaria/_DSC0151.png',
+    image: './assets/galaria/DSC0151.png',
   },
   {
     id: 27,
-    image: './assets/galaria/_DSC0156.png',
+    image: './assets/galaria/DSC0156.png',
   },
 
   {
     id: 28,
-    image: './assets/galaria/_DSC0165.png',
+    image: './assets/galaria/DSC0165.png',
   },
   {
     id: 29,
-    image: './assets/galaria/_DSC0166.png',
+    image: './assets/galaria/DSC0166.png',
   },
 
   {
     id: 30,
-    image: './assets/galaria/_DSC0170.png',
+    image: './assets/galaria/DSC0170.png',
   },
 
   {
     id: 31,
-    image: './assets/galaria/_DSC0172.png',
+    image: './assets/galaria/DSC0172.png',
   },
   {
     id: 32,
-    image: './assets/galaria/_DSC0177.png',
+    image: './assets/galaria/DSC0177.png',
   },
   {
     id: 33,
-    image: './assets/galaria/_DSC0181.png',
+    image: './assets/galaria/DSC0181.png',
   },
 
   {
     id: 34,
-    image: './assets/galaria/_DSC0186.png',
+    image: './assets/galaria/DSC0186.png',
   },
 
   {
     id: 35,
-    image: './assets/galaria/_DSC0189.png',
+    image: './assets/galaria/DSC0189.png',
   },
   {
     id: 36,
-    image: './assets/galaria/_DSC0190.png',
+    image: './assets/galaria/DSC0190.png',
   },
   {
     id: 37,
-    image: './assets/galaria/_DSC0195.png',
+    image: './assets/galaria/DSC0195.png',
   },
 
   {
     id: 39,
-    image: './assets/galaria/_DSC0201.png',
+    image: './assets/galaria/DSC0201.png',
   },
   {
     id: 40,
-    image: './assets/galaria/_DSC0204.png',
+    image: './assets/galaria/DSC0204.png',
   },
   {
     id: 41,
-    image: './assets/galaria/_DSC0206.png',
+    image: './assets/galaria/DSC0206.png',
   },
   {
     id: 42,
-    image: './assets/galaria/_DSC0208.png',
+    image: './assets/galaria/DSC0208.png',
   },
   {
     id: 43,
-    image: './assets/galaria/_DSC0214.png',
+    image: './assets/galaria/DSC0214.png',
   },
 
   {
     id: 44,
-    image: './assets/galaria/_DSC0219.png',
+    image: './assets/galaria/DSC0219.png',
   },
   {
     id: 45,
-    image: './assets/galaria/_DSC0229.png',
+    image: './assets/galaria/DSC0229.png',
   },
 ];
